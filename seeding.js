aeaec9331738526111f589e32bfc9cfa210a7ffa
Ameer Hamza <juttameerhamza@gmail.com>
2021-05-02 22:13:49 +0500

    remote changes

--- seeding.js
+++ seeding.js
@@ -24,7 +24,7 @@ const initSeeding = async () => {
             role: 'admin'
         });
 
-        console.log('-----------Admin created!--------------');
+        console.log('------------Admin created!--------------');
         console.log('Admin: ', user);
         process.exit(0);
     } catch (error) {
